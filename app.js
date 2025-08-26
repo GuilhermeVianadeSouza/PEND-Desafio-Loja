1d4bf20ab324621bbcb6e227a0e016f5edfbd4c1
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-08-26 08:12:26 -0300

    Finalizando temporariamente o Header. Iniciando a parte do JavaScript do programa. Ainda será necessário a implementação e consolidação das informações na tela e seu conteudo.

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 'use strict'
 
+import produtos from "./produtos_atualizados.json" with {type: "json"}
+
+function mostrarProdutos(){
+    const product = document.getElementById('card')
+
+    produtos.forEach(func =>{
+
+    const container = document.createElement('div')
+    container.classList.add('produto')
+    
+    const imagens = document.createElement('img')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    })
+
+}
