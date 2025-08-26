e9f5f182bbe41f227f851ebe2754d6e8d860dc4c
GUILHERME VIANA DE SOUZA <25132756@CFP127ALUNO.LOCAL>
2025-08-26 08:49:28 -0300

    Aplicando a imagem e textos dos produtos da loja na tela. Ainda necessário ajustes utilizando-se do CSS para uma melhor contenação visual e maiores ajustes em torno do contorno dos produtos. Ajustes requiridos. Versão: 1.5.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import produtos from "./produtos_atualizados.json" with {type: "json"}
 
 function mostrarProdutos(){
     const product = document.getElementById('card')
+    const caminhoImagem = './img/'
 
     produtos.forEach(func =>{
 
@@ -11,18 +12,33 @@ function mostrarProdutos(){
     container.classList.add('produto')
     
     const imagens = document.createElement('img')
+    imagens.src = caminhoImagem + func.imagem
+
+    const nome = document.createElement('h2')
+    nome.textContent = func.nome
+
+    const descricao = document.createElement('a')
+    descricao.textContent = func.descricao
+
+    const avaliacao = document.createElement('a')
+    avaliacao.textContent = func.classificacao
+
+    const category = document.createElement('a')
+    category.textContent = func.categoria
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    const preco = document.createElement('a')
+    preco.textContent = func.preco
+
+    container.appendChild(imagens)
+    container.appendChild(nome)
+    container.appendChild(descricao)
+    container.appendChild(avaliacao)
+    container.appendChild(category)
+    container.appendChild(preco)
+
+    product.appendChild(container)
     })
 
 }
+
+mostrarProdutos()
